3e87039d0edfef369f39b7fbb8a236c74f34b95d
Amit <Amit@Amit-PC.telus>
2020-02-12 19:17:12 -0700

    second commit

---
+++ selenium/src/main/java/selenium/selenium/Test.java
@@ -0,0 +1,18 @@
+package selenium.selenium;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+
+public class Test {
+public static void main(String orgs[])
+{
+	
+System.setProperty("webdriver.chrome.driver", "F:/eclipse/selenium/chromedriver_win32/chromedriver.exe");
+ChromeOptions options = new ChromeOptions();
+options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
+WebDriver driver = new ChromeDriver();
+driver.get("https://www.google.com");
+System.out.println("amit");
+}
+}
